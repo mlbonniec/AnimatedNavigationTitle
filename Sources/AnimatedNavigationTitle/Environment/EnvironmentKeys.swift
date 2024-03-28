2aaba7264cab8dffc3cdc87fd062479c2e233784
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-03-28 21:27:45 +0100

    feat(environment): create environment keys extensions

---
+++ Sources/AnimatedNavigationTitle/Environment/EnvironmentKeys.swift
@@ -0,0 +1,16 @@
+//
+//  EnvironmentKeys.swift
+//  AnimatedNavigationTitle
+//
+//  Created by Mathis Le Bonniec on 28/03/2024.
+//
+
+import SwiftUI
+
+struct AnimatedNavigationSafeAreaTopInsetKey: EnvironmentKey {
+  static let defaultValue: CGFloat = 0
+}
+
+struct AnimatedNavigationTitleContent: EnvironmentKey {
+  static let defaultValue: Text? = nil
+}
