9599b836dbe3d82fd07e8e16e35ca585462ac442
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-03-29 21:45:26 +0100

    feat(model): create OptionSet for animation type

---
+++ Sources/AnimatedNavigationTitle/Models/AnimatedNavigationTitleModel.swift
@@ -0,0 +1,23 @@
+//
+//  AnimatedNavigationTitleModel.swift
+//  AnimatedNavigationTitle
+//
+//  Created by Mathis Le Bonniec on 29/03/2024.
+//
+
+import SwiftUI
+
+public struct AnimatedNavigationTitleAnimationType: OptionSet {
+  // MARK: Properties
+  public let rawValue: Int
+
+  // MARK: Lifecycle
+  public init(rawValue: Int) {
+    self.rawValue = rawValue
+  }
+
+  // MARK: Values
+  public static let slide = AnimatedNavigationTitleAnimationType(rawValue: 1 << 0)
+  public static let opacity = AnimatedNavigationTitleAnimationType(rawValue: 1 << 1)
+  public static let all: AnimatedNavigationTitleAnimationType = [.opacity, .slide]
+}
